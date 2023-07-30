a2d87c99bd11125ae657e001249203187d84f7bd
yohanecheverri <johan0321@Gmail.com>
2023-07-29 20:46:04 -0500

    agregando estilos

--- src/index.js
+++ src/index.js
@@ -181,9 +181,9 @@ sectionMain.appendChild(articleSectionMain)
 
 const hotelesCard = async () => {
     const response = await fetch ("https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels");
-    // const data = await response.json();
-    // return data
-    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    return data
 }
 
 // articleSectionMain.appendChild(hotelesCard(data.name))
